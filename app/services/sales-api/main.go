7db831d50837822877ea0ae4781091f23b816cf9
anon <naixatwork@gmail.com>
2024-03-10 18:59:33 +0330

    feat(zarf)!: added dockerfile

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -10,6 +10,8 @@ import (
 	"syscall"
 )
 
+var build = "develop"
+
 func main() {
 	log, err := logger.New("SALES-API")
 	if err != nil {
@@ -29,7 +31,7 @@ func run(log *zap.SugaredLogger) error {
 
 	// -------------------------------------------------------------------------
 
-	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD", build)
 
 	// -------------------------------------------------------------------------
 
